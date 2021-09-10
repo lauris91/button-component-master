9e714a1e9a017f2cb8526ae1b33d4fdd3cb96e31
Lauris Zaicevs <lauris.zaicevs@abcsoftware.lv>
2021-09-10 18:54:15 +0300

    Add multiple props for Button component

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -3,14 +3,39 @@ import './button.scss'
 
 const classNames = require('classnames')
 
-const Button = ({ hover }) => {
-  console.log('class ', classNames({ foo: true, bar: true }))
+const Button = ({
+  variant = 'default',
+  color,
+  disableShadow,
+  children,
+  startIcon,
+  endIcon,
+  size = 'md',
+  hover,
+  ...others
+}) => {
   const classes = classNames({
     btn: true,
+    [`btn--${variant}`]: variant,
+    [`btn--color-default`]: !color,
+    [`btn--color-${color}`]: color,
+    'btn--disableShadow': disableShadow,
+    'btn--disabled': others.disabled,
+    'btn--startIcon': startIcon,
+    'btn--endIcon': endIcon,
+    [`btn--${size}`]: size,
     hover: hover,
   })
 
-  return <button className={classes}>Default</button>
+  const Icon = <span className='material-icons'>{startIcon || endIcon}</span>
+
+  return (
+    <button className={classes} {...others}>
+      {startIcon && Icon}
+      {children}
+      {endIcon && Icon}
+    </button>
+  )
 }
 
 export default Button
